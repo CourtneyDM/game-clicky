9d4927b27459d5319d14dff68e72c749c5e38d78
Courtney Montgomery <courtdmontgomery@gmail.com>
2018-06-21 23:56:51 -0500

    Changes

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Title from './components/Title';
 import dominoes from './dominoes.json';
 
 class App extends React.Component {
+     // Initialize state
      state = {
           dominoes,
           message: 'Click on a domino to begin!',
@@ -12,7 +13,7 @@ class App extends React.Component {
           topScore: 0
      };
 
-     // Whent the user guesses incorrectly, end game
+     // When the user guesses incorrectly, end game
      endGame = () => {
           if (this.state.score > this.state.topScore) {
                this.setState({
@@ -29,32 +30,42 @@ class App extends React.Component {
           return true;
      }
 
-     // 
+     // When clicked, check the count for each domino by id prop
      clicked = id => {
-          this.state.dominoes.find((key, index) => {
-               if (key.id === id) {
+
+          // Find the index of the domino and compare to id prop
+          this.state.dominoes.find((domino, index) => {
+
+               if (domino.id === id) {
+                    // If the domino at the index has a count of 0, increase it by 1
                     if (dominoes[index].count === 0) {
                          dominoes[index].count = dominoes[index].count + 1;
+
+                         // Update the score with each successful click
                          this.setState({ score: this.state.score + 1 }, function () {
                               console.log(this.state.score);
                          });
+
+                         // Shuffle the dominoes
                          this.state.dominoes.sort(() => Math.random() - 0.5)
                          return true;
-                    } else {
-                         this.endGame();
+                    }
+                    // Else, end the game
+                    else {
+                         return this.endGame();
                     }
                }
           });
      }
 
-     // Map over this.state.dominoes and render a Domino component for each domino object
+     // Map over dominoes and render a Domino component for each domino object
      render() {
           return (
                <Wrapper>
                     <Title
                          message={ this.state.message }
                          score={ this.state.score }
-                         topScore={ this.state.topScore }>Watch The Dominoes
+                         topScore={ this.state.topScore }>Domino Memory Challenge
                     </Title>
                     { this.state.dominoes.map(domino => (
                          <Domino
--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -2,14 +2,13 @@ import React from 'react';
 import './Title.css';
 
 const Title = props => (
-    <div className="header">
-        <h1>{ props.children }</h1>
-        <h3>{ props.message } </h3>
-        <div className="scores">
-            Score: { props.score } High Score: { props.topScore }
-        </div>
-    </div>
-
+     <div className="header">
+          <h1>{ props.children }</h1>
+          <h3>{ props.message } </h3>
+          <div className="scores">
+               Score: { props.score } High Score: { props.topScore }
+          </div>
+     </div>
 );
 
 export default Title;
