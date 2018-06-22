8ac6b477b6ffb6d3edfdd684221a0725f2dc7d1d
Courtney Montgomery <courtdmontgomery@gmail.com>
2018-06-21 23:19:18 -0500

    updates

--- src/App.js
+++ src/App.js
@@ -1,37 +1,74 @@
 import React from 'react';
-import Domino from './components/Domino/Domino';
-import Wrapper from './components/Wrapper/Wrapper';
-import Title from './components/Title/Title';
+import Domino from './components/Domino';
+import Wrapper from './components/Wrapper';
+import Title from './components/Title';
 import dominoes from './dominoes.json';
 
 class App extends React.Component {
-    // Set state of dominoes
-    state = {
-        dominoes
-    };
+     state = {
+          dominoes,
+          message: 'Click on a domino to begin!',
+          score: 0,
+          topScore: 0
+     };
 
-    // TODO: Create a function that will randomly shuffle dominoes on the screen using Math.Random
+     // Whent the user guesses incorrectly, end game
+     endGame = () => {
+          if (this.state.score > this.state.topScore) {
+               this.setState({
+                    topScore: this.state.score
+               }, () => console.log(this.state.topScore));
+          }
 
-    // shuffleDominoes = id => {
-    //     const dominoes = this.state.dominoes();
-    // };
+          this.state.dominoes.forEach(domino => {
+               domino.count = 0;
+          });
 
-    render() {
-        return (
-            <Wrapper>
-                <Title>Watch That Domino</Title>
-                { this.state.dominoes.map(domino => (
-                    <Domino
-                        // shuffleDominoes={ this.shuffleDominoes }
-                        id={ domino.id }
-                        key={ domino.id }
-                        name={ domino.name }
-                        image={ domino.image }
-                    />
-                )) }
-            </Wrapper>
-        );
-    }
+          alert("Game over. Want to play again?");
+          this.setState({ score: 0 });
+          return true;
+     }
+
+     // 
+     clicked = id => {
+          this.state.dominoes.find((key, index) => {
+               if (key.id === id) {
+                    if (dominoes[index].count === 0) {
+                         dominoes[index].count = dominoes[index].count + 1;
+                         this.setState({ score: this.state.score + 1 }, function () {
+                              console.log(this.state.score);
+                         });
+                         this.state.dominoes.sort(() => Math.random() - 0.5)
+                         return true;
+                    } else {
+                         this.endGame();
+                    }
+               }
+          });
+     }
+
+     // Map over this.state.dominoes and render a Domino component for each domino object
+     render() {
+          return (
+               <Wrapper>
+                    <Title
+                         message={ this.state.message }
+                         score={ this.state.score }
+                         topScore={ this.state.topScore }>Watch The Dominoes
+                    </Title>
+                    { this.state.dominoes.map(domino => (
+                         <Domino
+                              clicked={ this.clicked }
+                              id={ domino.id }
+                              key={ domino.id }
+                              name={ domino.name }
+                              image={ domino.image }
+                         />
+                    )) }
+
+               </Wrapper>
+          );
+     }
 }
 
 export default App;
--- src/components/Domino/Domino.js
+++ src/components/Domino/Domino.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import './Domino.css';
 
-const Domino = props => (
-    <div className="card">
-        <div className="img-container">
-            <img
-                alt={ props.name }
-                src={ props.image } />
-        </div>
-    </div>
-);
+const Domino = props => {
+     return (
+          <div className='domino' onClick={ () => props.clicked(props.id) }>
+               <img
+                    alt={ props.name }
+                    src={ props.image }
+               />
+          </div>
+     );
+}
 
 export default Domino;
